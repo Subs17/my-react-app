dbc6651c20375a7411caada974eae78970f0ebec
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-01-11 21:48:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles/shared.css'
 import './styles/styles.css';
 import './styles/vars.css';
+import './styles/PageStyles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
