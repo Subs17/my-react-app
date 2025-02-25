8d2da668933c01e9d8db86465120c7d149668d52
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-02-25 12:52:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@fullcalendar/react', 
+      '@fullcalendar/daygrid', 
+      '@fullcalendar/timegrid', 
+      '@fullcalendar/interaction'
+    ],
+  },
+  server: {
+    proxy: {
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
