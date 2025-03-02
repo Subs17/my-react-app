1b5dad246b58fabe4be2ca4319a2061639182d86
Subs17 <98551288+Subs17@users.noreply.github.com>
2025-03-02 15:21:08 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,47 @@
-const Navbar = () => {
+import PropTypes from 'prop-types';
+
+const Navbar = ({tabs = []}) => {
     return (
       <div className="navbar">
-      <h1>Elderly Care Portal</h1>
+        <div className='navbar-logo-title'>
+        <img 
+        src='/src/assets/icons/Elderlycare-logo.png'
+        alt='Elderly Care Logo'
+        className='navbar-logo'
+        />
+      <h1 className='navbar-title'>Elderly Care Portal</h1>
+      </div>
       <div className="navbar-links-container">
         <ul className="navbar-links">
-          <li>
-          <span>About Us</span>
-          </li>
-          <li>
-            <span>FAQ</span>
-          </li>
-          <li>
-            <span>Support</span>
-          </li>
+          {tabs.map((tab, index) => (
+            <li key={index} className="navbar-link"> 
+              <a href={tab.link}
+              onClick={(e) => {
+                if (tab.action) {
+                  e.preventDefault();
+                  tab.action();
+                }
+              }}>{tab.name}</a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
     );
   };
-  
+
+Navbar.propTypes = {
+    tabs: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        link: PropTypes.string.isRequired,
+        action: PropTypes.func, //Optional action for logout
+      })
+    ).isRequired,
+};
+
+Navbar.defaultProps = {
+    tabs: [],
+};
+
   export default Navbar;
